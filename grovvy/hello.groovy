29d02b2f9820abee3faf75b47758a65a608dc217
hpym365 <hpym365@gmail.com>
2017-03-02 23:11:45 +0800

    java对的Groovy调用

--- grovvy/hello.groovy
+++ grovvy/hello.groovy
@@ -1,6 +1,10 @@
-println "Welcome to $language"
-String a = "hello"
-def map =  "$map"
-println a
-println map.getAt("java")
-return "The End"
+//println "Welcome to $language"
+//String a = "hello"
+//def map =  "$map"
+//println a
+//println map.getAt("java")
+//return "The End"
+
+def hello(param1,param2) {
+	return "the params is $param1 and $param2"
+}
