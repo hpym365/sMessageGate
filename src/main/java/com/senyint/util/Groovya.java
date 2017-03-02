005a77237011fcc5892e8ec9d20df826f0488145
hpym365 <hpym365@gmail.com>
2017-03-02 17:29:35 +0800

    踩坑里了  springboot可能带groovy包 但是还不全 结果ScriptEngineManager.getEngineByName(groovy)报错

---
+++ src/main/java/com/senyint/util/Groovya.java
@@ -0,0 +1,13 @@
+package com.senyint.util;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+public class Groovya{
+
+	public static void main(String[] args) {
+		System.out.println("123");
+	}
+	
+}
